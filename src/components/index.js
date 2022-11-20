7f7b35d884cce2242ff411021faedd18a87abb33
HaSS-1 <85417909+HaSS-1@users.noreply.github.com>
2022-11-20 01:06:26 +0100

    Hero Section created with Css files & Header Hero Footer Moved to new folder

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+
+export { default as Container } from "./Container/Container"
+export { default as NavItem } from "./NavItem/NavItem"
+export { NavItemDropDown } from "./NavItem/NavItem"
+
+
+
---
+++ src/sections/Hero/Hero.js
@@ -0,0 +1,18 @@
+import "./Hero.css"
+
+const Hero = () => {
+    return (
+        <div className="hero-main">
+            <div className="hero-text">
+                <h6 className="hero-subtitle">Welcome To NewBorn</h6>
+                <h4 className="hero-title"><em>Discover</em> My New Born Projects Here</h4>
+
+                <div className="main-button">
+                    <a href="browse.html">Discover Now</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
